6f699b9a621119fab1cbec26200c280baeb626e9
ilariamotta <ilaria.motta93@gmail.com>
2025-12-17 16:24:46 +0100

    feat: rotte CRUD completate

--- app.js
+++ app.js
@@ -1,7 +1,16 @@
 import express from "express"
+import  postsRouter from "./routers/post.js"
 
 const app = express();
 const port = 3000;
+
+
+app.get("/", (req, res) => {
+    console.log("rotta /");
+    res.send("test")
+})
+
+app.use("/posts", postsRouter)
 
 app.listen(port, () => {
     console.log("Il server è in ascolto sulla porta " + port)
---
+++ routers/post.js
@@ -0,0 +1,50 @@
+import express from "express";
+
+const router = express.Router();
+
+//INDEX
+
+router.get("/", (req, res) => {
+    res.send("Lista videogiochi")
+})
+
+//SHOW
+
+router.get("/:id", (req, res) => {
+    const id = req.params.id;
+    console.log("singolo")
+    res.send("Videogioco singolo " + id)
+})
+
+// STORE
+
+router.post("/", (req, res) => {
+    console.log("creo nuovo gioco");
+    res.send("Creo nuovo videogioco")
+})
+
+// UPDATE
+
+router.put("/:id", (req, res) => {
+    console.log("modifico videogioco")
+    const id = req.params.id;
+    res.send("Aggiorno videogioco " + id)
+})
+
+// MODIFY
+
+router.patch("/:id", (req, res) => {
+    console.log("aggiorno parzialmente videogioco")
+    const id = req.params.id;
+    res.send("Aggiorno parzialmente videogioco " + id)
+})
+
+// DESTROY
+
+router.delete("/:id", (req, res) => {
+    console.log("elimino videogioco")
+    const id = req.params.id;
+    res.send("Elimino videogioco " + id)
+})
+
+export default router
